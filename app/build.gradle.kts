40acfc4c4d592fcde32a297bb50cab5b40033e71
Hooramk <166182286+Hooramk@users.noreply.github.com>
2026-09-25 00:02:56 +0330

    Bump complete ending edition to v0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "ir.masirservat.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.5.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17

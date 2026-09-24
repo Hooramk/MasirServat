5d7f03639b710d67b87eae46eddbef922a941ec5
Hooramk <166182286+Hooramk@users.noreply.github.com>
2026-09-24 21:19:01 +0330

    Bump story-driven version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "ir.masirservat.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17

2b3c9ca20eba13b28aacbb9c92610e850fd0b412
Hooramk <166182286+Hooramk@users.noreply.github.com>
2026-09-24 21:34:12 +0330

    Add story metadata to events

--- app/src/main/java/ir/masirservat/app/EventData.java
+++ app/src/main/java/ir/masirservat/app/EventData.java
@@ -1,8 +1,12 @@
 package ir.masirservat.app;
 import java.util.ArrayList;
 import java.util.List;
+
 public class EventData {
-    public String id,title,description;
-    public List<Choice> choices=new ArrayList<>();
-    public static class Choice { public String id,title,lesson; }
+    public String id, chapter, speaker, title, description;
+    public List<Choice> choices = new ArrayList<>();
+
+    public static class Choice {
+        public String id, title;
+    }
 }

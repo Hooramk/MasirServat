73b81e11b321f30244e00580afe633a414a9cfd4
Hooramk <166182286+Hooramk@users.noreply.github.com>
2026-09-24 20:40:53 +0330

    Add event model

---
+++ app/src/main/java/ir/masirservat/app/EventData.java
@@ -0,0 +1,8 @@
+package ir.masirservat.app;
+import java.util.ArrayList;
+import java.util.List;
+public class EventData {
+    public String id,title,description;
+    public List<Choice> choices=new ArrayList<>();
+    public static class Choice { public String id,title,lesson; }
+}
